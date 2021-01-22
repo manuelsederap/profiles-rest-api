32ed6100f1a98352ddf970964582e9deaa3de469
Manuel Paredes <manuelsederap@gmail.com>
2021-01-22 21:38:28 +0800

    change-database-settings-to-mysql

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# LOCAL MariaDB
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'profiles_api',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
 }
 
