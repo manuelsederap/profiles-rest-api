501b1525633e7534f926685be8c3dc1e9a8fed0a
Manuel Paredes <manuelsederap@gmail.com>
2021-01-27 23:49:12 +0800

    added user profile feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from profiles_api import models
 
 
 class UpdateOwnProfile(permissions.BasePermission):
@@ -6,7 +7,19 @@ class UpdateOwnProfile(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profle"""
+        # import pdb; pdb.set_trace()
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to update their own status"""
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.id == request.user.id
+        return obj.user_profile.id == request.user.id        
